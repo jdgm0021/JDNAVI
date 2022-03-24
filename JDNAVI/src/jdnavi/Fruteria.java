79956a5a84530feb993b5af36fc75e8fa48a07cf
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-03-24 14:45:00 +0100

    Añadir clase fruteria y eliminar mainclass fruteria

--- JDNAVI/src/jdnavi/Fruteria.java
+++ JDNAVI/src/jdnavi/Fruteria.java
@@ -10,12 +10,5 @@ package jdnavi;
  * @author Josedavid
  */
 public class Fruteria {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
