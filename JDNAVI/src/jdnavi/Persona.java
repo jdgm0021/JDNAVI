e30b721ae2ce3527d0fe4436e182efe3a7a95b3c
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-03-31 12:35:31 +0200

    Constructor de persona

--- JDNAVI/src/jdnavi/Persona.java
+++ JDNAVI/src/jdnavi/Persona.java
@@ -3,6 +3,14 @@ package jdnavi;
 
 public class Persona {
     String nombre;                  //nombre de la persona
-    boolean genero;                 //genero de la persona
+    boolean genero;                 //genero de la persona; True -> Hombre; False -> Mujer
     int edad;                       //edad de la persona
+
+    public Persona(String nombre, boolean genero, int edad) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+    }
+    
+    
 }
