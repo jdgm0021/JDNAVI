97e1a2a8ac4f1f446722d59dbcc4bfb7be7356a3
Ignacio Pérez Sánchez <ips0005@alu.medac.es>
2022-03-31 12:26:13 +0200

    Variables de la clase persona

--- JDNAVI/src/jdnavi/Persona.java
+++ JDNAVI/src/jdnavi/Persona.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jdnavi;
 
-/**
- *
- * @author Josedavid
- */
 public class Persona {
-    
+    String nombre;                  //nombre de la persona
+    boolean genero;                 //genero de la persona
+    int edad;                       //edad de la persona
 }
