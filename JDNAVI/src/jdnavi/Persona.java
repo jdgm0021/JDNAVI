829a54bb3dfb55887ae3e2c9f1e4ec82da9eda00
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-03-31 12:42:57 +0200

    Genero aleatorio

--- JDNAVI/src/jdnavi/Persona.java
+++ JDNAVI/src/jdnavi/Persona.java
@@ -6,9 +6,13 @@ public class Persona {
     boolean genero;                 //genero de la persona; True -> Hombre; False -> Mujer
     int edad;                       //edad de la persona
 
-    public Persona(String nombrePersona, boolean generoPersona, int edadPersona) {
+    public Persona(String nombrePersona, int edadPersona) {
         this.nombre = nombrePersona;
-        this.genero = generoPersona;
+        if ((Math.random() * ((2 + 1) - 1) + 1) == 1) {
+            this.genero = true;
+        }else{
+            this.genero = false;
+        }
         this.edad = edadPersona;
     }
     
