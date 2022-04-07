c5c7cc52aa4faa83b5cdc0564e956175287cadab
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-04-07 14:14:29 +0200

    Añadir variables de Fruteria

--- JDNAVI/src/jdnavi/Fruteria.java
+++ JDNAVI/src/jdnavi/Fruteria.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jdnavi;
 
-/**
- *
- * @author Josedavid
- */
+import java.util.ArrayList;
+
 public class Fruteria {
+    
+    private String nombre;
+    private ArrayList<Persona> cola;
+    private ArrayList<Persona> personasAtendidas;
     
 }
