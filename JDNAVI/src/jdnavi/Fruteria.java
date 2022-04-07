bf7c1499eee19754e879178ea67e7a6586f2de9b
Ignacio Pérez Sánchez <ips0005@alu.medac.es>
2022-04-07 13:35:23 +0200

    Constructor de Fruteria

--- JDNAVI/src/jdnavi/Fruteria.java
+++ JDNAVI/src/jdnavi/Fruteria.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 
 public class Fruteria {
     
-    private String name;
+    private String nombre;
     private ArrayList<Persona> cola;
     private ArrayList<Persona> personasAtendidas;
     
-    
+    public Fruteria(String name){
+        this.nombre=name;
+        this.cola= new ArrayList<>();
+        this.personasAtendidas= new ArrayList<>();
+    }
 }
