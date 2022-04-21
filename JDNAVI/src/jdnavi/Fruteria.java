8ac58c6efe72a74a4ebdf526414ac9fd841f2c6a
unknown <vmf0002@alu.medac.es>
2022-04-21 14:00:18 +0200

    Añadir la funcionalidad de personas atendidas

--- JDNAVI/src/jdnavi/Fruteria.java
+++ JDNAVI/src/jdnavi/Fruteria.java
@@ -23,6 +23,7 @@ public class Fruteria {
     
     public void atenderCliente(){
         System.out.println("Se ha atendido al cliente " + cola.get(0).nombre);
+        personasAtendidas.add(cola.get(0));
         cola.remove(0);
         
     }
