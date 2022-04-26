6973b203d1f1225cd8fc0eaeb0e1b8a82077ca3f
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-04-26 14:40:26 +0200

    Aniadir comentarios

--- JDNAVI/src/jdnavi/Persona.java
+++ JDNAVI/src/jdnavi/Persona.java
@@ -170,18 +170,18 @@ public class Persona {
             }
         }
     }
-    public void abandonarCola(){
-        ArrayList<Persona> cola;
-        cola = fruteria.getCola();
-        cola.remove(fruteria.getCola().indexOf(this));
-        fruteria.setCola(cola);
+    public void abandonarCola(){//Una persona va a poder abandonar la cola solo haciendo p1.abandonarCola(); es decir, sin usar Fruteria
+        ArrayList<Persona> cola; 
+        cola = fruteria.getCola(); //Usamos la cola de Fruteria en Persona
+        cola.remove(fruteria.getCola().indexOf(this)); //Eliminamos de esta cola a this
+        fruteria.setCola(cola); //Cambiamos la cola por esta en fruteria
     }
     
     public void dejarPasar(){
         ArrayList<Persona> cola;
-        cola = fruteria.getCola();
-        cola.add((fruteria.getCola().indexOf(this))+2,this);
-        cola.remove(fruteria.getCola().indexOf(this));
-        fruteria.setCola(cola);
+        cola = fruteria.getCola(); //Usamos la cola de Fruteria en Persona
+        cola.add((fruteria.getCola().indexOf(this))+2,this); //Nos añadimos 2 puestos atrás para dejar pasar a una persona
+        cola.remove(fruteria.getCola().indexOf(this)); //Nos eliminamos de la cola para no estar repetidos y dejamos al que está detrás
+        fruteria.setCola(cola); //Cambiamos la cola por esta en fruteria
     }
 }
