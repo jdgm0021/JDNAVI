837b663a98123ec060d1a73fb6651848d597c6b4
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-04-28 14:31:38 +0200

    Añadir switch

--- JDNAVI/src/jdnavi/NewMain.java
+++ JDNAVI/src/jdnavi/NewMain.java
@@ -8,10 +8,36 @@ public class NewMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean salir = false;
+        int opcion;
         System.out.print("Introduce un nombre para tu fruteria: ");
         Fruteria fruteria = new Fruteria(scanner.nextLine());
-        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("BIENVENIDO A TU NUEVA FRUTERIA '" + fruteria.getNombre() + "'.");
         do {
+            System.out.println("");
+            imprimirMenu();
+            opcion = scanner.nextInt();
+            
+            switch(opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("ERROR: Selecciona una opción correcta...");
+                    break;
+            }
             
         } while (!salir);
         
