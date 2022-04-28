db34a8e704042f75c65fe289c884922e967b044d
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-04-28 14:22:20 +0200

    Añadir imprimirMenu

--- JDNAVI/src/jdnavi/NewMain.java
+++ JDNAVI/src/jdnavi/NewMain.java
@@ -7,4 +7,14 @@ public class NewMain {
         boolean salir = false;
     }
     
+    public static void imprimirMenu(){
+        System.out.println("Selecciona una opción:");
+        System.out.println("1.- Crear un cliente");
+        System.out.println("2.- Añadir un cliente a la cola");
+        System.out.println("3.- Atender un cliente");
+        System.out.println("4.- Adelantar un cliente");
+        System.out.println("5.- Atrasar un cliente");
+        System.out.println("6.- Mostrar los clientes atendidos");
+        System.out.println("7.- Mostrar los clientes no atendidos");
+    }
 }
