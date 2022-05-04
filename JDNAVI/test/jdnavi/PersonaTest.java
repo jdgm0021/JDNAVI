6f2c34eeb584cf7d93d1ffca5fcefd08deb621fb
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-05-04 16:34:50 +0200

    Añadir fruteria para test persona

--- JDNAVI/test/jdnavi/PersonaTest.java
+++ JDNAVI/test/jdnavi/PersonaTest.java
@@ -5,10 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PersonaTest {
-    
+    Fruteria fruteria;
     public PersonaTest() {
+        fruteria = new Fruteria("Fruteria Test");
     }
-
+    //Para probar esta clase primero debemos instanciar una Fruteria.
     @Test
     public void testAbandonarCola() {
     }
