e6b2266c3090539939fb6bd070b4196c80cfa225
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-05-04 17:08:09 +0200

    Arreglar test abandonar cola

--- JDNAVI/test/jdnavi/PersonaTest.java
+++ JDNAVI/test/jdnavi/PersonaTest.java
@@ -7,20 +7,8 @@ import static org.junit.Assert.*;
 public class PersonaTest {
     //Para probar esta clase primero debemos instanciar una Fruteria.
     Fruteria fruteria;
-    /*Persona cliente1;
-    Persona cliente2;
-    Persona cliente3;*/
     public PersonaTest() {
         fruteria = new Fruteria("Fruteria Test");
-        /*cliente1 = new Persona();
-        cliente2 = new Persona("Jose David", 18, true);
-        cliente3 = new Persona("Maritto", 76, true);
-        fruteria.añadirCliente(cliente1);
-        fruteria.añadirCliente(cliente2);
-        fruteria.añadirCliente(cliente3);
-        fruteria.añadirCliente("Raul Mendoza", 19, true);
-        fruteria.añadirCliente("Maria del Pilar", 35, false);
-        fruteria.añadirCliente("Jazmin del Mar", 56, false);*/
     }
     
     @Test
@@ -28,13 +16,15 @@ public class PersonaTest {
         //1.- Preparar
         fruteria.añadirCliente("Umtiti", 24, true);
         fruteria.añadirCliente("Maria", 45, false);
-        Persona cliente2 = new Persona();
+        Persona cliente2 = new Persona("Antonio", 19, true);
         fruteria.añadirCliente(cliente2);
+        Persona cliente3 = new Persona("Manuel", 67, false);
+        fruteria.añadirCliente(cliente3);
         //2.- Probar
         cliente2.abandonarCola();
         //3.- Verificar
-        assertEquals( 2, fruteria.getCola().size());
-        //assertEquals(cliente3, fruteria.getCola().get(1));
+        assertEquals( 3, fruteria.getCola().size());
+        //assertEquals(cliente3, fruteria.getCola().get(2));
     }
 
     /*@Test
