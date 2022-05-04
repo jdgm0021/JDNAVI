d959e5cd0751e01171a6dda1f26ea30752bfcf7f
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-05-04 17:25:46 +0200

    Crear test abandonar cola maximo

--- JDNAVI/test/jdnavi/PersonaTest.java
+++ JDNAVI/test/jdnavi/PersonaTest.java
@@ -26,6 +26,21 @@ public class PersonaTest {
         assertEquals( 3, fruteria.getCola().size());
         //assertEquals(cliente3, fruteria.getCola().get(2));
     }
+    
+    @Test
+    public void testAbandonarColaMaximo() {
+        //1.- Preparar
+        Persona cliente2 = new Persona("Antonio", 19, true);
+        fruteria.añadirCliente(cliente2);
+        Persona cliente3 = new Persona("Manuel", 67, false);
+        fruteria.añadirCliente(cliente3);
+        //2.- Probar
+        cliente2.abandonarCola();
+        cliente3.abandonarCola();
+        cliente3.abandonarCola();
+        //3.- Verificar
+        assertEquals( 0, fruteria.getCola().size());
+    }
 
     /*@Test
     public void testDejarPasar() {
