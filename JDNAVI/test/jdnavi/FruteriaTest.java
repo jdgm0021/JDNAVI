00ac3bb38aaf0994008ba93a1545b25ea35b701f
José David Gómez Moreno <jgm0021@alu.medac.es>
2022-05-04 16:04:22 +0200

    Añadir Persona test

--- JDNAVI/test/jdnavi/FruteriaTest.java
+++ JDNAVI/test/jdnavi/FruteriaTest.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jdnavi;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Josedavid
- */
 public class FruteriaTest {
     
     public FruteriaTest() {
---
+++ JDNAVI/test/jdnavi/PersonaTest.java
@@ -0,0 +1,26 @@
+
+package jdnavi;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonaTest {
+    
+    public PersonaTest() {
+    }
+
+    /**
+     * Test of abandonarCola method, of class Persona.
+     */
+    @Test
+    public void testAbandonarCola() {
+    }
+
+    /**
+     * Test of dejarPasar method, of class Persona.
+     */
+    @Test
+    public void testDejarPasar() {
+    }
+    
+}
